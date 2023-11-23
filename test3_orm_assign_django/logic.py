792b378153a056bd8e24211222c6b17f463e40f2
Tommy Guy <riguy@microsoft.com>
2023-11-23 04:58:52 -0800

    test 3 init

---
+++ test3_orm_assign_django/logic.py
@@ -0,0 +1,21 @@
+from django.views import View, JsonResponse
+from django.contrib.auth import LoginRequiredMixin
+from django.urls import reverse
+
+from uuid import uuid4
+
+from .myclass import ShareRequest
+
+
+class DocumentClusterCreateShareView(LoginRequiredMixin, View):
+
+    def post(self, request, pk):
+        obj = get_object_or_404(ShareRequest, active=True, owner=self.request.user, id=pk)
+
+        share_guid = f"{uuid4()}__{uuid4()}".replace("-", "")
+        obj.share_link=share_guid
+        obj.save()
+
+        full_share_url = request.build_absolute_uri(reverse("share_landing", kwargs={'guid': share_guid}))
+        return JsonResponse({'share': full_share_url})
+    
