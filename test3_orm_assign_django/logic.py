834603ae34eea4633f4ec85f8d6d61421369b583
Tommy Guy <riguy@microsoft.com>
2023-11-23 04:59:16 -0800

    test 3 change guid

--- test3_orm_assign_django/logic.py
+++ test3_orm_assign_django/logic.py
@@ -12,7 +12,7 @@ class DocumentClusterCreateShareView(LoginRequiredMixin, View):
     def post(self, request, pk):
         obj = get_object_or_404(ShareRequest, active=True, owner=self.request.user, id=pk)
 
-        share_guid = f"{uuid4()}__{uuid4()}".replace("-", "")
+        share_guid = uuid4()
         obj.share_link=share_guid
         obj.save()
 
