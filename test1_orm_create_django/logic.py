5ecc9cea06c13fb1a06ba2b52eaccc50b6bba76a
Tommy Guy <riguy@microsoft.com>
2023-11-22 19:20:25 -0800

    First commit for test 1

---
+++ test1_orm_create_django/logic.py
@@ -0,0 +1,25 @@
+from django.views import View, JsonResponse
+from django.contrib.auth import LoginRequiredMixin
+from django.urls import reverse
+
+from uuid import uuid4
+
+from .myclass import DocumentCluster, ShareRequest
+
+
+class DocumentClusterCreateShareView(LoginRequiredMixin, View):
+
+    def post(self, request, pk):
+        obj = get_object_or_404(DocumentCluster, active=True, owner=self.request.user, id=pk)
+
+        share_guid = f"{uuid4()}__{uuid4()}"
+        ShareRequest.objects.create(
+            owner=self.request.user,
+            shared_object='privateuploads.models.DocumentCluster',
+            shared_pk=obj.pk,
+            share_link=share_guid
+        )
+
+        full_share_url = request.build_absolute_uri(reverse("share_landing", kwargs={'guid': share_guid}))
+        return JsonResponse({'share': full_share_url})
+    
